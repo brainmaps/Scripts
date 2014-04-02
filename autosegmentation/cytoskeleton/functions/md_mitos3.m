832f0fb7fab2692bbbfd90125662ec6d43b5f7d9
mdraw <mdraw.gh@gmail.com>
2014-04-02 23:48:25 +0200

    Update md_mitos3.m
    v2.0
    - removed disk I/0 because this belongs in seperate "functions"
    -- md_mitos3 now operates only in system memory and does not touch hard disks anymore
    -- use e.g. md_volread and md_volwrite if you want disk input and output
    - added comments concerning reliability
    - updated defaults

--- autosegmentation/cytoskeleton/functions/md_mitos3.m
+++ autosegmentation/cytoskeleton/functions/md_mitos3.m
@@ -1,58 +1,49 @@
-function mitos = md_mitos3(inputPath,outputPath,hsize,sigma,threshold,minPx)
+function mitos = md_mitos3(volume,hsize,sigma,threshold,minPx)
 %MD_MITOS3 Roughly detects mitochondria by using their relative darkness in EM images.
 %
-% v1.0
+% v2.0
 %
-% Based on 3D gaussian blur, intensity thresholding and noise reduction by opening.
+% Based on 3D gaussian blur, intensity thresholding and region size limits.
 %
 % Similar to md_mitos2, but it uses a 3D gaussian blur and 3D opening instead
-% and it handles stacks of TIF images instead of just 2D images.
+% and it handles 3D image volumes instead of just 2D images.
 % It is potentially more accurate than md_mitos2, but it takes much more
 % time to calculate.
 %
+% Note that this function actually detects large dark structures in general
+% and does not take the specific form of mitochondria into account, so it is
+% currently not very reliable, e.g. PSDs are often mistaken for mitochondria.
+%
 % SYNOPSIS
-%   mitos = md_mitos3(inputPath,outputPath,hsize,sigma,threshold,minPx)
-%   mitos = md_mitos3(inputPath,outputPath,hsize,sigma,threshold)
-%   mitos = md_mitos3(inputPath,outputPath,hsize,sigma)
-%   mitos = md_mitos3(inputPath,outputPath,hsize)
-%   mitos = md_mitos3(inputPath,outputPath)
-%   mitos = md_mitos3(inputPath)
+%   mitos = md_mitos3(volume,hsize,sigma,threshold,minPx)
+%   mitos = md_mitos3(volume,hsize,sigma,threshold)
+%   mitos = md_mitos3(volume,hsize,sigma)
+%   mitos = md_mitos3(volume,hsize)
+%   mitos = md_mitos3(volume)
 %
 % INPUT
-%   inputPath (string): Path that contains the data in TIF format. (The files
-%       should have names like "0000.tif", "0001.tif", and so on.)
-%   outputPath (string): Directory that the results should be written into.
-%       If you do not want the function to write any files, use 'none' as
-%       its value. Default: 'none'
+%   volume (double): Image volume that should be processed, e.g. one loaded
+%       by md_readvol (grayscale, values between 0 and 1).
 %   hsize (uint8): "hsize" argument for the gaussian blur filter.
 %       Note that this argument is a vector that can determine the filter size
 %       in X,Y and Z direction independently. Consider changing the
 %       relative values to adapt to non-isometric datasets. Default: [5,5,5].
-%   sigma (double): "sigma" argument for the gaussian blur filter. Default: 20.
-%   threshold (double): intensity threshold for the distinction of mitochondria from
-%       other image contents. Default: 0.25
+%   sigma (double): "sigma" argument for the gaussian blur filter. Default: 2.
+%   threshold (double): intensity threshold for the distinction of mitochondria
+%       from other image contents. Default: 0.25
 %   minPx (uint8): Determines the minimum size of connected volumes.
 %       Can reduce false positives, but if set too high, it can lead to
-%       false negatives. Default: 600
+%       false negatives. Default: 561
 %
 % OUTPUT
-%   mitos: binary map of the mitochondria
+%   mitos: binary map of the mitochondria (currently not very reliable)
 
 %defaults
-if ~exist('outputPath','var'), outputPath = 'none'; end
 if ~exist('hsize','var'), hsize = [5,5,5]; end
-if ~exist('sigma','var'), sigma = 20; end
+if ~exist('sigma','var'), sigma = 2; end
 if ~exist('threshold','var'), threshold = .25; end
-if ~exist('minPx','var'), minPx = 600; end % Smallest mito found in dataset is 561 px
+if ~exist('minPx','var'), minPx = 561; end % Smallest mito found in dataset is 561 px
 
-% read all TIF filenames from inputPath
-fileNames = dir(strcat(inputPath,'/*.tif'));
-
-% read volume from TIF files, convert to double precision for better filter accuracy
-volume = zeros(512,512,length(fileNames));
-for k = 1:length(fileNames)
-    volume(:,:,k) = im2double(imread(fullfile(inputPath,fileNames(k).name)));
-end
 
 % apply a 3D gaussian blur filter.
 volumeBlur = smooth3(volume,'gaussian',hsize,sigma);
@@ -66,12 +57,5 @@ mitosE = bwareaopen(mitosT,minPx);
 
 % fill holes in marked regions
 mitos = imfill(mitosE,'holes');
-
-if (~strcmp(outputPath,'none')) % write results to outputPath if it is not 'none'
-    for k = 1:length(fileNames)
-        imwrite(mitos(:,:,k),fullfile(outputPath,fileNames(k).name),'Compression','none')
-        % using no compression because ImageJ currently cannot read compressed binary TIFs
-    end
-end
 
 end
