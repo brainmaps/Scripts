866578cb7ad6b270b3ab28568d10fbfca8ff90a6
mdraw <mdraw.gh@gmail.com>
2014-04-09 20:28:58 +0200

    Update md_volread.m
    v2.0
    - now compatible with all image sizes (not only 512x512)
    - added support for logical (binary) images
    - added additional error handling
    - improved documentation, error messages and comments

--- autosegmentation/cytoskeleton/functions/md_volread.m
+++ autosegmentation/cytoskeleton/functions/md_volread.m
@@ -1,12 +1,14 @@
 function volume = md_volread(inputPath,arrayType)
-%MD_VOLREAD Create a 3D array from a directory containing numbered TIFs. 
+%MD_VOLREAD Create a 3D array from a directory containing numbered TIFs.
 %
-% v1.0
+% v2.0
 %
-% Supports output as double or uint8 arrays (default is double).
+% X and Y dimensions have to be consistent within the TIF stack.
+% Supports input as double, uint8 or logical arrays (default is double).
 %
-% "double" means that intensity values are doubles between 0 and 1.
-% "uint8" means that intensity values are unsigned integers between 0 and 255.
+% - "double" means that intensity values are doubles between 0 and 1.
+% - "uint8" means that intensity values are unsigned integers between 0 and 255.
+% - "logical" means that all intensity values are logical values (0 or 1).
 %
 % SYNOPSIS
 %   volume = md_volread(inputPath,arrayType)
@@ -14,38 +16,52 @@ function volume = md_volread(inputPath,arrayType)
 %
 % INPUT
 %   inputPath (string): Path that contains the data in TIF format. (The files
-%       should have names like "0000.tif", "0001.tif", and so on.)
-%   arrayType (string): Data type for the volume. Only "double" and "uint8"
-%       are supported. Set it to "uint8" to get a uint8 array. Default: double.
+%       should have names like e.g. "0000.tif", "0001.tif", and so on.)
+%   arrayType (string): Data type for the volume. Supported values are
+%       "double", "uint8" and "logical". Default: double.
 %
 % OUTPUT
-%   volume: 3D volume array
+%   volume: 3D volume array that stores the individual 2D images along the
+%       third dimension, ordered by their original filename.
 
 % defaults
 if ~exist('arrayType','var'), arrayType = 'double'; end
 
 % check if path is valid
 if exist(inputPath,'dir') ~= 7
-    error(['"',inputPath,'" is not a valid path.']);
+    error(['"',inputPath,'" is not a valid path. Aborting...']);
 end
 
 % read all TIF file names from inputPath
 files = dir([inputPath,'/*.tif']);
 
+% abort if there are no TIFs in the directory
+if size(files,1) == 0
+    error(['"',inputPath,'" does not contain any TIF images! Aborting...']);
+end
+
+% determine the X and Y dimensions of the TIF stack by checking the first image
+[xSize,ySize] = size(imread(fullfile(inputPath,files(1).name)));
+
 % read volume from TIF files
 if strcmp(arrayType,'double')
-    volume = zeros(512,512,length(files));
+    volume = zeros(xSize,ySize,length(files));
     for k = 1:length(files)
         volume(:,:,k) = im2double(imread(fullfile(inputPath,files(k).name)));
     end
 elseif strcmp(arrayType,'uint8')
-    volume = zeros(512,512,length(files),'uint8');
+    volume = zeros(xSize,ySize,length(files),'uint8');
+    for k = 1:length(files)
+        volume(:,:,k) = imread(fullfile(inputPath,files(k).name));
+    end
+elseif strcmp(arrayType,'logical')
+    volume = false(xSize,ySize,length(files));
     for k = 1:length(files)
         volume(:,:,k) = imread(fullfile(inputPath,files(k).name));
     end
 else
     error(['"',arrayType,'" is not a supported array type. ',...
-        'Try "double" or "uint8".']);
+        'Try "double", "uint8" or "logical". Aborting...']);
 end
 
 end
