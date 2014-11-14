ea066b8330dc5b1fc6160d3661e676687c067f63
jhennies <julianhennies@hotmail.de>
2014-11-14 12:29:24 +0100

    Classes re-structured

--- GUIs/Viewer/v1/classes/Visualization.m
+++ GUIs/Viewer/v1/classes/Visualization.m
@@ -1,16 +1,27 @@
 classdef Visualization
     
     properties
-        currentPosition % [x y z]
-        displaySize     % [x y z]
+        data
+        currentPosition     % [x y z]
+        displaySize         % [x y z]
         bSectionalPlanes
         bOverlayObjects
         spacerSize
         anisotropicInterpolationType
+        anisotropyFactor    % [x y z]
         bufferDelete
     end
     
+    properties (Dependent)
+        
+        roundedPosition
+
+    end
+    
     methods
+        
+        %% Initialization
+        
         function vis = Visualization(varargin)
             vis.currentPosition = varargin{1};
             vis.displaySize = varargin{2};
@@ -18,8 +29,19 @@ classdef Visualization
             vis.bOverlayObjects = varargin{4};
             vis.spacerSize = varargin{5};
             vis.anisotropicInterpolationType = varargin{6};
-            vis.bufferDelete = varargin{7};
+            vis.anisotropyFactor = varargin{7};
+            vis.bufferDelete = varargin{8};
         end
+        
+        %% Get-sets
+        
+        function value = get.roundedPosition(this)
+            value = round(this.currentPosition);
+        end
+        
+        %% Operations
+        
+        
     end
     
 end
